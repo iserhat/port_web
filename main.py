cef2d081b60e563dbe1fab7466f76209e85aabb2
iserhat <iserhatercan@gmail.com>
2024-05-08 23:28:06 -0400

    First of the project that contains main components of the portfolio web-site

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for, request
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from flask_bootstrap import Bootstrap5
+import requests
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+Bootstrap5(app=app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
